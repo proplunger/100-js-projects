ff675b5fe4833dd5e0caa652caa52344ed09bc59
proplunger <104715666+proplunger@users.noreply.github.com>
2023-04-04 21:47:19 +0530

    commit

--- 15-rock-paper-scissors/app.js
+++ 15-rock-paper-scissors/app.js
@@ -2,9 +2,15 @@ const Rock = document.querySelector(".Rock");
 const Paper = document.querySelector(".Paper");
 const Scissors = document.querySelector(".Scissors");
 
+const wins = document.querySelector(".Wins");
+const draws = document.querySelector(".Draws");
+const loss = document.querySelector(".Loss");
+
+let w = 0, l = 0, d = 0;
 
 // Play function
-const handlePlay  = (PlayerMove) => {
+const handlePlay  = (e) => {
+    const PlayerMove = e.currentTarget.className;
     const arr = ["Rock", "Paper", "Scissors"];
     const index = Math.floor(Math.random() * 3);
     const move = arr[index];
@@ -24,10 +30,21 @@ const handlePlay  = (PlayerMove) => {
         else ans = "Win";
     }
     console.log(ans);
+    if(ans == "Win"){
+        w += 1;
+        wins.innerText = `Wins: ${w}`;
+    }else if(ans == "Loss"){
+        l += 1;
+        loss.innerText = `Loss: ${l}`;
+    }else{
+        d += 1;
+        draws.innerText = `Draws: ${d}`;
+    }
 }
 
-Rock.addEventListener("click", handlePlay("Rock"));
-Paper.addEventListener("click", handlePlay("Paper"));
-Scissors.addEventListener("click", handlePlay("Scissors"));
+
+Rock.addEventListener("click", handlePlay);
+Paper.addEventListener("click", handlePlay);
+Scissors.addEventListener("click", handlePlay);
 
 
